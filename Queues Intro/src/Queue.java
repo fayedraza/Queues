4c508660f36b68ad6ece1af5dcb6b1a1286cd22a
fayedraza <fayedraza@fayeds-mbp.fios-router.home>
2019-08-18 18:57:17 -0400

    Commit

---
+++ Queues Intro/src/Queue.java
@@ -0,0 +1,18 @@
+//created by Fayed Raza
+import java.util.*;
+
+public class Queue {
+	//purpose is to introduce queue
+	public static void main(String ...args) {
+  PriorityQueue<String> q = new  PriorityQueue<String>();
+
+  q.offer("a");
+  q.offer("b");
+  q.offer("c");
+  q.poll(); //removes the first one to be offered
+  System.out.printf("%s ", q); 
+  System.out.printf("%s ", q.peek()); //returns the first one to be offered
+  
+
+	}
+}
